a3e6e5fdd240308f8bfcdd2ca075931b9c7a9837
Lasitha Silva <lasithasilva@lasithas02.home>
2019-07-18 23:30:06 +0800

    Completed the module

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+         public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
